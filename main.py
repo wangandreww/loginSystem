695a344cdc56db266906c0332c88fb0996a7ea91
wangandreww <59212728+wangandreww@users.noreply.github.com>
2020-07-31 18:58:53 -0400

    Created profile and register pages, buttons work

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect,session,url_for
+from flask_mysqldb import MySQL
+import MySQLdb
 
 app = Flask(__name__)
 
@@ -7,10 +9,14 @@ app = Flask(__name__)
 def index():
     return render_template("login.html")
 
+@app.route('/profile')
+def login():
+    return render_template("profile.html")
 
-@app.route('/register')
-def new_user:
-    return render_template("register.html")
+
+@app.route('/new')
+def new_user():
+    return render_template("register2.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
