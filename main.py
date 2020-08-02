e4764eee69e703a9deba615beb09b010361f6838
wangandreww <59212728+wangandreww@users.noreply.github.com>
2020-08-02 03:25:48 -0400

    created connection with mysql

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
-from flask import Flask, render_template,redirect,session,url_for
+from flask import Flask, render_template, request, redirect, session, url_for
 from flask_mysqldb import MySQL
 import MySQLdb
 
 app = Flask(__name__)
+app.secret_key = "12341234"
 
-@app.route('/')
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "PiguPigu149"
+app.config["MYSQL_DB"] = "login"
 
+db = MySQL(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        if 'username' in request.form and 'password' in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute("SELECT * FROM logininfo WHERE email=%s AND password=%s", (username, password))
+            info = cursor.fetchone()
+            if info is not None:
+            if info['email'] == username and info['password'] == password:
+                return "login successfull"
+            else:
+                return "login unsuccesfull, please register"
     return render_template("login.html")
 
-@app.route('/profile')
-def login():
-    return render_template("profile.html")
-
-
-@app.route('/new')
-def new_user():
-    return render_template("register2.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
-
