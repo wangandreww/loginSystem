13a18b205d61c5a120ef811379e3a95e08fd7883
wangandreww <59212728+wangandreww@users.noreply.github.com>
2020-08-05 03:00:37 -0400

    login system and create user are functional

--- main.py
+++ main.py
@@ -24,12 +24,32 @@ def index():
             info = cursor.fetchone()
             if info is not None:
                 if info['email'] == username and info['password'] == password:
-                    return "login successfull"
+                    session['loginsuccess'] = True
+                    return redirect(url_for('profile'))
             else:
-                return "login unsuccesfull, please register"
+                return redirect(url_for('index'))
     return render_template("login.html")
 
 
+@app.route('/new', methods=['GET', 'POST'])
+def new_user():
+    if request.method == 'POST':
+            if "one" in request.form and "two" in request.form and "three" in request.form:
+                username = request.form['one']
+                email = request.form['two']
+                password = request.form['three']
+                cursor = db.connection.cursor(MySQLdb.cursors.DictCursor)
+                cursor.execute("INSERT INTO login.logininfo(name,password,email)VALUES(%s,%s,%s)", (username, password, email))
+                db.connection.commit()
+                return redirect(url_for('index'))
+    return render_template("register.html")
+
+
 @app.route('/new/profile')
+def profile():
+    if session['loginsuccess']:
+        return render_template("profile.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
